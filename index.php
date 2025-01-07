e051d5228d9de453b6507c8466d523b47dd8aa59
! fusY <onlyoneniqtophileee@gmail.com>
2025-01-07 08:57:11 +0800

    adding nama and nisn

--- index.php
+++ index.php
@@ -1,3 +1,12 @@
 <?php
 
+// - simpan nama dan nisn kalian ke dalam sebuah variabel
+$nama = 'Nabil Maulana Yusuf';
+$nisn = '0064260633';
+
+//- tampilkan/cetak variabel tersebut ke layar
+echo 'Nama  : ' ,$nama ;
+echo '<br>'; 
+echo 'NISN : ' ,$nisn;
+
 ?>
